efcec33baf468e92d86cbe2528dcc8e901412266
abqani2 <161646868+abqani2@users.noreply.github.com>
2024-03-19 14:44:38 +0100

    Add files via upload

---
+++ lightbox.js
@@ -0,0 +1,28 @@
+"use strict";
+   
+/*
+      Image List
+
+      Filename:lightbox.js
+      Author: Abdiqani Hirsi
+*/
+
+
+let lightboxTitle = "Choose the activity you want to participate in";
+
+// Names of the image files shown in the slideshow
+let imgFiles = ["football.jpg", "Dancing.jpg", "Caltivate.jpg", "Artist.jpg",
+                "Music-Concert.jpg",]
+
+// Captions associated with each image
+let imgCaptions = new Array(5);
+imgCaptions[0]="Football";
+imgCaptions[1]="Dancing"; 
+imgCaptions[2]="Caltivate"; 
+imgCaptions[3]="Artist"; 
+imgCaptions[4]="Music-Concert";
+//imgCaptions[5]="Tennis";
+
+
+// Count of images in the slideshow
+let imgCount = imgFiles.length;
