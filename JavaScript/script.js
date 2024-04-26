3f992e71051e2b160cbcd0e04746596dbc623c22
IngeborgBF <144833921+IngeborgBF@users.noreply.github.com>
2024-04-26 09:56:48 +0200

    Add smooth scrolling functionality to the 'Back to Top' button (and some pictures)

---
+++ JavaScript/script.js
@@ -0,0 +1,14 @@
+// Sanftes Hochscrollen
+document.getElementById("top").addEventListener("click", hochscrollen); 
+
+function hochscrollen() {
+    var scrolling = setInterval(
+        function(){
+            window.scrollBy(0,-20); 
+
+            if(document.documentElement.scrollTop ==0){
+                window.clearInterval(scrolling); 
+            }
+        },
+        10); 
+}
